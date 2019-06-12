78ac22559660c5841efd99391b0f03c405e1ed5a
Luiza <luitays@mail.ru>
2019-06-12 11:55:59 +0300

    added functionality with bookmarks

--- seed/index.js
+++ seed/index.js
@@ -1,9 +1,8 @@
-const { sequelize, User, Song, Bookmark } = require('../src/models')
+const { sequelize, User, Song } = require('../src/models')
 
-const Promise = require('bluebird')
 const songs = require('./songs.json')
 const users = require('./users.json')
-const bookmarks = require('./bookmarks.json')
+// const bookmarks = require('./bookmarks.json')
 
 sequelize.sync({force:true})
     .then( async function () {
@@ -19,9 +18,9 @@ sequelize.sync({force:true})
             })
         )
 
-        await Promise.all(
-            bookmarks.map(bookmark => {
-                Bookmark.create(bookmark)
-            })
-        )
+        // await Promise.all(
+        //     bookmarks.map(bookmark => {
+        //         Bookmark.create(bookmark)
+        //     })
+        // )
     })
--- src/controllers/BookmarksController.js
+++ src/controllers/BookmarksController.js
@@ -10,6 +10,7 @@ module.exports = {
           UserId: userId
         }
       })
+    
       res.send(bookmark)
     } catch (err) {
       res.status(500).send({
@@ -19,10 +20,25 @@ module.exports = {
   },
   async addBookmark (req, res) {
     try {
-      const bookmark = req.body
-      await Bookmark.create(bookmark)
-      res.send(bookmark)
+      const {songId, userId} = req.body
+      const bookmark = await Bookmark.findOne({
+        where: {
+          SongId: songId,
+          UserId: userId
+        }
+      })
+      if (bookmark) {
+        return res.status(400).send({
+          error: 'you already have this set as a bookmark'
+        })
+      }
+      const newBookmark = await Bookmark.create({
+        SongId: songId,
+        UserId: userId
+      })
+      res.send(newBookmark)
     } catch (err) {
+      console.log(err)
       res.status(500).send({
         error: 'An error has occured trying to create the bookmark'
       })
@@ -31,7 +47,11 @@ module.exports = {
   async deleteBookmark (req, res) {
     try {
       const {bookmarkId} = req.params
-      const bookmark = await Bookmark.findById(bookmarkId)
+      const bookmark = await Bookmark.findOne({
+        where: {
+          id: bookmarkId
+        }
+      })
       await bookmark.destroy()
       res.send(bookmark)
     } catch (err) {
